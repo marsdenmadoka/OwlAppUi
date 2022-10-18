faebfca64883d368eb5b10d8ee4f57d8f9bf12e8
marsdenmadoka <marsmadoka98@gmail.com>
2022-10-18 17:36:01 +0300

    added new files

--- app/src/main/java/com/madoka/instagramuicompose/ui/courses/MyCourses.kt
+++ app/src/main/java/com/madoka/instagramuicompose/ui/courses/MyCourses.kt
@@ -5,23 +5,85 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.madoka.instagramuicompose.ui.theme.BlueTheme
+
 
 @Composable
-fun   MyCourses(message: String, from: String) {
-    Column {
-        Text(
-            text = message,
-            fontSize = 36.sp,
-        )
-        Text(
-            text = from,
-            fontSize = 24.sp,
+fun MyCourses(
+    courses: List<Course>,
+    selectCourse: (Long) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier) {
+        item {
+            Spacer(Modifier.windowInsetsTopHeight(WindowInsets.statusBars))
+        }
+        item {
+            CoursesAppBar()
+        }
+        itemsIndexed(
+            items = courses,
+            key = { _, course -> course.id }
+        ) { index, course ->
+            MyCourse(
+                course = course,
+                index = index,
+                selectCourse = selectCourse
+            )
+        }
+    }
+}
+
+@Composable
+fun MyCourse(
+    course: Course,
+    index: Int,
+    selectCourse: (Long) -> Unit
+) {
+    Row(modifier = Modifier.padding(bottom = 8.dp)) {
+        val stagger = if (index % 2 == 0) 72.dp else 16.dp
+        Spacer(modifier = Modifier.width(stagger))
+        CourseListItem(
+            course = course,
+            onClick = { selectCourse(course.id) },
+            shape = RoundedCornerShape(topStart = 24.dp),
+            modifier = Modifier.height(96.dp)
         )
     }
 }
 
-@Preview(showBackground = false)
+@Preview(name = "My Courses")
 @Composable
-fun  MyCourses() {
-    MyCourses ("my courses!", "- from Emma")
+private fun MyCoursesPreview() {
+    BlueTheme {
+        MyCourses(
+            courses = courses,
+            selectCourse = { }
+        )
+    }
 }
+
+
+
+
+
+//
+//@Composable
+//fun   MyCourses(message: String, from: String) {
+//    Column {
+//        Text(
+//            text = message,
+//            fontSize = 36.sp,
+//        )
+//        Text(
+//            text = from,
+//            fontSize = 24.sp,
+//        )
+//    }
+//}
+//
+//@Preview(showBackground = false)
+//@Composable
+//fun  MyCourses() {
+//    MyCourses ("my courses!", "- from Emma")
+//}
